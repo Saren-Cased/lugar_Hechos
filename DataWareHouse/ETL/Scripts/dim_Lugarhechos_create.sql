d57d968a53e5da40852312d948dbe8cf41fc75f9
Saren-Cased <urdi.cased.5@gmail.com>
2025-06-14 22:05:42 -0500

    Update dim_Lugarhechos_create.sql

--- DataWareHouse/ETL/Scripts/dim_Lugarhechos_create.sql
+++ DataWareHouse/ETL/Scripts/dim_Lugarhechos_create.sql
@@ -5,13 +5,13 @@
 IF OBJECT_ID('dbo.FAC_Lugarhechos', 'U') IS NULL
 BEGIN
 	CREATE TABLE dbo.FAC_Lugarhechos (
-		IdLugarhechos int NOT NULL,
+		IdLugarhechos int IDENTITY(1,1) NOT NULL,
 		FechaHecho date NOT NULL,
-		CodMpio int NOT NULL,
-		CodStio int NOT NULL,
+		CodMpio int NULL,
+		CodStio int NULL,
 		IdRiesgo int NOT NULL,
-		IdSexo int NOT NULL,
-		TipoRuta nvarchar(30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+		IdSexo int NULL,
+		TipoRuta nvarchar(30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		CONSTRAINT FAC_Lugarhechos_PK PRIMARY KEY (IdLugarhechos),
 		CONSTRAINT FAC_Lugarhechos_DIM_Fechas_FK FOREIGN KEY (FechaHecho) REFERENCES DataWareHouseSub.dbo.DIM_Fechas(FechaHecho),
 		CONSTRAINT FAC_Lugarhechos_DIM_Municipios_FK FOREIGN KEY (CodMpio) REFERENCES DataWareHouseSub.dbo.DIM_Municipios(CodMpio),
